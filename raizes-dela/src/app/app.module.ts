f03230bdb0e15f1ae05b7de48c06118153b5aefc
ddsp-pinheiro <ddsp.pinheiro@gmail.com>
2021-03-22 15:24:56 -0300

    feat(componente):implementando login e cadastro

--- raizes-dela/src/app/app.module.ts
+++ raizes-dela/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { RodapeComponent } from './rodape/rodape.component';
 import { MenuComponent } from './menu/menu.component';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RodapeComponent,
-    MenuComponent
+    MenuComponent,
+    LoginComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
