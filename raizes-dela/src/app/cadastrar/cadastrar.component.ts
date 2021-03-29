2de8446b47902c3816109daff09c433820a88eee
Lucas <lucasloiolaalmeida@gmail.com>
2021-03-29 18:00:12 -0300

    feat(cadastro): consumo da API do cadastro

---
+++ raizes-dela/src/app/cadastrar/cadastrar.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
+
+@Component({
+  selector: 'app-cadastrar',
+  templateUrl: './cadastrar.component.html',
+  styleUrls: ['./cadastrar.component.css']
+})
+export class CadastrarComponent implements OnInit {
+  user: Usuario = new Usuario
+  confirmarSenha: string
+  //tipoUsuario: boolean
+  constructor(
+    private authService: AuthService,
+    private router: Router
+    //private alert
+  ) { }
+  ngOnInit() {
+    window.scroll(0,0)
+  }
+  confirmSenha(event: any) {
+    this.confirmarSenha = event.target.value
+  }
+  // tipoUser(event: any) {
+  //   this.tipoUser = event.target.value
+  // }
+  cadastrar() {
+    //this.user.tipoVendedor = this.tipoUsuario
+    if(this.user.senha != this.confirmarSenha) {
+      alert("As senhas estão incorretas.")
+    } else {
+      this.authService.cadastrar(this.user).subscribe((resp: Usuario)=> {
+        this.user = resp
+        if(this.user == null) {
+          alert ('Usuário já existe, por favor escolha outro.')
+        } else {
+          this.router.navigate(['/login'])
+          alert ('Usuário cadastrado com sucesso!')
+        }
+      })
+    }
+  }
+}
+
---
+++ raizes-dela/src/app/model/Produto.ts
@@ -0,0 +1,13 @@
+import { Categoria } from "./Categoria"
+import { Usuario } from "./Usuario"
+
+export class Produto{
+    public id: number
+    public nome: string
+    public descricao: string
+    public estoque: number
+    public imagem: string
+    public valor: number
+    public categoria: Categoria
+    public usuario: Usuario
+}
