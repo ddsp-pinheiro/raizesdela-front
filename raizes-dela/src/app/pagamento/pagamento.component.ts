ea4a90ef9df8cef71570612aa4948152565fe5f0
Debora <debvm@hotmail.com>
2021-03-31 11:10:26 -0300

    feat(Pagamento): Base HTML, CSS e TS

--- raizes-dela/src/app/pagamento/pagamento.component.ts
+++ raizes-dela/src/app/pagamento/pagamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pagamento',
@@ -7,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagamentoComponent implements OnInit {
 
-  constructor() { }
+  //formulario endereco
+  endereco = document.querySelector("#endereco")
+  cidade = document.querySelector ("#cidade")
+  cep = document.querySelector("#cep")
+  //formulario cartao
+   nomeCartao= document.querySelector("#nomeCartao")
+   noCartao= document.querySelector("#noCartao")
+   cpf= document.querySelector("#cpf")
+   cvv= document.querySelector("#cvv")
+  //validações
+  parte1OK = false
+  parte2Ok = false
 
-  ngOnInit(): void {
+  constructor(private router:Router) { }
+
+  ngOnInit(){
+    window.scroll(0,0)
   }
 
+  //  validaBoleto(){
+  //     if(this.endereco.value == "" || this.cidade != null || this.cep != null)
+  //     {
+  //       this.parte1OK = true
+
+  //     } else{
+  //       alert("Preencha corretamente o formulário")
+  //     }
+  // }
+
+  // validaCartao(){
+  //     if(this.parte1OK == false ){
+  //       alert("Preencha corretamente o endereço")
+  //     } else if(this.nomeCartao == null || this.cpf == null || this.noCartao == null || this.cvv == null) {
+  //       alert("Preencha corretamente os dados do cartão")
+  //     } else{
+  //       this.parte2Ok = true
+  //       alert("Pagamento realizado com sucesso! Aguarde email de confirmação.")
+  //       this.router.navigate (['/home'])
+  //     }
+  // }
+
+  sucesso(){
+    alert("Pagamento realizado com sucesso! Aguarde email de confirmação.")
+    this.router.navigate (['/home'])
+  }
 }
